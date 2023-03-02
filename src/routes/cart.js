1b3176d72068e56234c759ec41c772666a22e09c
phong <phongit1995@gmail.com>
2023-03-02 17:48:38 +0700

    update cart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 const cartController = require('../app/controllers/CartController');
 
-router.use('/', cartController.cart)
+router.get('/', cartController.cart)
+router.post('/add',cartController.addCart);
+router.post('/remove',cartController.removeCart)
 module.exports = router;
