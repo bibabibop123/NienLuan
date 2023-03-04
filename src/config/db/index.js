be6a40f4f5c1f467ab7be34489a7ed4bbdc696a1
bibabibop123 <thaison13021998@gmail.com>
2023-03-04 15:40:01 +0700

    add mongodb

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/f8_education_dev');
+        await mongoose.connect('mongodb+srv://thuhan445566:vtEa52dDY0ubsxVS@shop.fskablk.mongodb.net/shop');
         console.log('ok');
     } catch (error) {
         console.log('failed to connect');
