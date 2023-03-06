83b2af374d13d7f950759f65556f81a5a5192cf3
phong <phongit1995@gmail.com>
2023-03-06 13:16:28 +0700

    update cart

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -7,27 +7,36 @@ class CartController {
         res.render('cart');
     }
     async addCart(req,res,next){
+        const id = req.params.id;
+        const product = await Course.findById(id);
+        console.log('product',product);
+        if(!product){
+            return res.redirect('/');
+        }
         const cart = req.session.cart || [];
         console.log(cart);
         
-        const cartExist = cart.find((item)=>item.id ==req.body.id);
+        const cartExist = cart.find((item)=>item.id ===id);
         if(cartExist){
             let cartNew = cart.map((item)=>{
-                if(item.id===req.body.id){
+                if(item.id==id){
                     return {...item,quality:item.quality+1}
                 }
+                return item;
             })
+            console.log('cartNew',cartNew);
             req.session.cart = cartNew;
         }else {
             cart.push({
-                id:'123',
-                image:'item',
-                name: 'name hay',
+                id: product._id,
+                image:product.img,
+                name: product.name_content,
                 quality:1
             })
             req.session.cart = cart;
         }
-        res.send('hello, phong nguyen');
+        req.flash('message', ' Thêm sản phẩm thành công vào giỏ hàng !!!');
+        return res.redirect(`/detail/${product.name_content}`)
     }
 
     async removeCart(req,res){}
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -6,5 +6,6 @@ const cartController = require('../app/controllers/CartController');
 
 router.get('/', cartController.cart)
 router.post('/add',cartController.addCart);
+router.get('/add-cart/:id',cartController.addCart);
 router.post('/remove',cartController.removeCart)
 module.exports = router;
