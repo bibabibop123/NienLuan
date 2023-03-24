fc67ea3a1edee47efc6a868e3f2f35865a06731a
phong <phongit1995@gmail.com>
2023-03-24 11:04:44 +0700

    update payment

--- src/app/controllers/PaymentController.js
+++ src/app/controllers/PaymentController.js
@@ -2,7 +2,15 @@ const Course = require('../models/Course');
 
 class PaymentController {
     async payment ( req, res, next) {
+        // if(!req.user){
+        //     req.flash('message', 'Vui lòng đăng nhập để tiến hành thanh toán !!!');
+        //     return res.redirect('/login');
+        // }
         return res.render('payment');
+    }
+
+    async paymentConfirm(){
+        
     }
 }
 
