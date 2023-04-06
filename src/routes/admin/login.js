fe09ec863eed86f6bfed4bb1d949f1d16c8ba1ab
phong <phongit1995@gmail.com>
2023-04-06 09:59:38 +0700

    add admin base

--- src/routes/admin/login.js
+++ src/routes/admin/login.js
@@ -4,6 +4,6 @@ const router = express.Router();
 router.get('/', (req,res)=>{
     console.log('login');
     // res.send('login');
-    res.render('admin/login',{layout:'admin'});
+    res.render('admin/login',{layout:false});
 })
 module.exports = router;
