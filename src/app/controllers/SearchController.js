0397e8ca6b8342747217381839d03c3f3b362882
phong <phongit1995@gmail.com>
2023-04-07 10:27:59 +0700

    add search

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -2,8 +2,13 @@ const Course = require('../models/Course');
 
 class SearchController {
     async search ( req, res, next) {
-
-        return res.render('search');
+        const {keyword} = req.query;
+        if(!keyword){
+            res.redirect('/');
+        }
+        const courseSearch = await Course.find({name_content:{$regex: keyword, $options: 'i'}}).lean();
+        console.log('courseSearch',courseSearch);
+        return res.render('search',{data:courseSearch});
     }
 }
 
